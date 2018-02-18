50b864d673b531370b3f8d46b953555197054955
Zack Kollar <rallokkcaz@gmail.com>
2018-02-17 20:24:17 -0800

    Initial commit.

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express') // Import express
+const morgan = require('morgan') // Use morgan to log express events
+const chalk = require('chalk') // Chalk for console output
+
+const app = express() // Create an express server
+
+app.use(morgan('combined')) // Setup the morgan middle ware.
+
+// This is a bare bones middleware.
+app.use((req, res, next) => {
+    console.log(req.body)
+    next() // Next is required to pass this object down the chain.
+})
+
+const PORT = 3000 // Port constant, will come from a configuration file.
+
+// Start our express server and pass a callback 
+// that runs when the server is online.
+app.listen(PORT, () => {
+    console.log(chalk.green(`Listening on port ${PORT}...`))
+})
